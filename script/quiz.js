6e5e82f86d46d487ed4d7d4ef20a63ab89a46a28
Silver <gramakov27@gmail.com>
2022-09-14 20:04:12 +0300

    добавил иконку, мета-теги, убрал лишнее теги форм, правки инпутов и кнопок, поправил адаптив, уменьшение шрифтов на 600px, поправил логику смены шагов

--- script/quiz.js
+++ script/quiz.js
@@ -11,18 +11,14 @@ $(document).ready(function () {
     }
 
     btnNext.on('click', () => {
-        if (step === 1 && name.value.length >= 2
+        if (step === 1
+            && name.value.length >= 2
             && $('input[name="phone"]').is(':not(:placeholder-shown)')
             && city.value.length >= 3)
             changeStep();
-        else if (step === 2 && $('input[name="step-2"]').is(':checked')) changeStep();
-        else if (step === 3 && $('input[name="step-3"]').is(':checked')) changeStep();
-        else if (step === 4 && $('input[name="step-4"]').is(':checked')) changeStep();
-        else if (step === 5 && $('input[name="step-5"]').is(':checked')) changeStep();
-        else if (step === 6 && $('input[name="step-6"]').is(':checked')) changeStep();
-        else if (step === 7 && $('input[name="step-7"]').is(':checked')) changeStep();
-        else if (step === 8 && $('input[name="step-8"]').is(':checked')) changeStep();
-        else if (step === 9 && $('input[name="step-9"]').is(':checked')) changeStep();
+        else if (step < 10 && $('input[name="step-' + step + '"]').is(':checked')) {
+            changeStep();
+        }
         else if (step === 10 && $('input[name="step-10"]').is(':checked')) {
             changeStep();
             $('.quiz__bottom-part').fadeOut(300);
